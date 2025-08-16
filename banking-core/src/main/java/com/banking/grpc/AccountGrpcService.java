8569682cd5acdf72ffc7f5d8ffc5ee9e795b7a6d
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2025-08-16 09:12:51 +0000

    grpcservice: create account

---
+++ banking-core/src/main/java/com/banking/grpc/AccountGrpcService.java
@@ -0,0 +1,47 @@
+package com.banking.grpc;
+
+import com.banking.dto.CreateAccountDto;
+import com.banking.model.Account;
+import com.banking.service.AccountService;
+import io.grpc.stub.StreamObserver;
+import com.banking.model.AccountType;
+
+import java.math.BigDecimal;
+
+public class AccountGrpcService extends AccountServiceGrpc.AccountServiceImplBase{
+    private final AccountService accountService;
+
+    public AccountGrpcService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    public void createAccount(CreateAccountRequest request, StreamObserver<AccountResponse> responseObserver) {
+        try {
+            CreateAccountDto dto = new CreateAccountDto(
+                    request.getOwnerId(),
+                    request.getCurrency(),
+                    new BigDecimal(request.getInitialBalance()),
+                    AccountType.valueOf(request.getAccountType())
+            );
+
+            Account account = accountService.createAccount(dto);
+            responseObserver.onNext(mapToAccountResponse(account));
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    private AccountResponse mapToAccountResponse(Account account) {
+        return AccountResponse.newBuilder()
+                .setAccountId(account.getAccountId())
+                .setAccountNumber(account.getNumber())
+                .setOwnerId(account.getOwnerId())
+                .setBalance(account.getBalance().toString())
+                .setCurrency(account.getCurrency())
+                .setAccountType(account.getAccountType().name())
+                .setStatus(account.getStatus().name())
+                .build();
+    }
+}
